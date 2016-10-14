6d0e0655bef9b812fec05de555bc98c4fb5a277e
wahhe <yuanhe326@ed.amdsb.ca>
2016-10-14 13:17:32 -0400

    Add project files.

---
+++ EB Cash/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace EB_Cash
+{
+    partial class EBCash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EBCash));
+            this.EBlabel = new System.Windows.Forms.Label();
+            this.Titlelabel = new System.Windows.Forms.Label();
+            this.chickenB = new System.Windows.Forms.Label();
+            this.CBox = new System.Windows.Forms.TextBox();
+            this.beefB = new System.Windows.Forms.Label();
+            this.BBox = new System.Windows.Forms.TextBox();
+            this.Softdrink = new System.Windows.Forms.Label();
+            this.Fries = new System.Windows.Forms.Label();
+            this.FBox = new System.Windows.Forms.TextBox();
+            this.Poutine = new System.Windows.Forms.Label();
+            this.PBox = new System.Windows.Forms.TextBox();
+            this.SBox = new System.Windows.Forms.TextBox();
+            this.calculateP = new System.Windows.Forms.Button();
+            this.subTlabel = new System.Windows.Forms.Label();
+            this.stPlabel = new System.Windows.Forms.Label();
+            this.taxlabel = new System.Windows.Forms.Label();
+            this.tPlabel = new System.Windows.Forms.Label();
+            this.totallabel = new System.Windows.Forms.Label();
+            this.totalPlabel = new System.Windows.Forms.Label();
+            this.spacelabel = new System.Windows.Forms.Label();
+            this.tenderedlabel = new System.Windows.Forms.Label();
+            this.dollarlabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.calchange = new System.Windows.Forms.Button();
+            this.changelabel = new System.Windows.Forms.Label();
+            this.changeP = new System.Windows.Forms.Label();
+            this.Printbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EBlabel
+            // 
+            this.EBlabel.AutoSize = true;
+            this.EBlabel.Font = new System.Drawing.Font("Blackoak Std", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EBlabel.Location = new System.Drawing.Point(294, -1);
+            this.EBlabel.Name = "EBlabel";
+            this.EBlabel.Size = new System.Drawing.Size(156, 50);
+            this.EBlabel.TabIndex = 0;
+            this.EBlabel.Text = "EB";
+            // 
+            // Titlelabel
+            // 
+            this.Titlelabel.Font = new System.Drawing.Font("Trajan Pro", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titlelabel.Location = new System.Drawing.Point(269, 37);
+            this.Titlelabel.Name = "Titlelabel";
+            this.Titlelabel.Size = new System.Drawing.Size(214, 44);
+            this.Titlelabel.TabIndex = 1;
+            this.Titlelabel.Text = "EatBurger ";
+            // 
+            // chickenB
+            // 
+            this.chickenB.AutoSize = true;
+            this.chickenB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chickenB.Location = new System.Drawing.Point(27, 81);
+            this.chickenB.Name = "chickenB";
+            this.chickenB.Size = new System.Drawing.Size(132, 20);
+            this.chickenB.TabIndex = 3;
+            this.chickenB.Text = "Chicken Burger";
+            // 
+            // CBox
+            // 
+            this.CBox.Location = new System.Drawing.Point(204, 83);
+            this.CBox.Name = "CBox";
+            this.CBox.Size = new System.Drawing.Size(35, 20);
+            this.CBox.TabIndex = 4;
+            // 
+            // beefB
+            // 
+            this.beefB.AutoSize = true;
+            this.beefB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beefB.Location = new System.Drawing.Point(53, 107);
+            this.beefB.Name = "beefB";
+            this.beefB.Size = new System.Drawing.Size(106, 20);
+            this.beefB.TabIndex = 5;
+            this.beefB.Text = "Beef Burger";
+            // 
+            // BBox
+            // 
+            this.BBox.Location = new System.Drawing.Point(204, 109);
+            this.BBox.Name = "BBox";
+            this.BBox.Size = new System.Drawing.Size(35, 20);
+            this.BBox.TabIndex = 6;
+            // 
+            // Softdrink
+            // 
+            this.Softdrink.AutoSize = true;
+            this.Softdrink.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Softdrink.Location = new System.Drawing.Point(77, 135);
+            this.Softdrink.Name = "Softdrink";
+            this.Softdrink.Size = new System.Drawing.Size(82, 20);
+            this.Softdrink.TabIndex = 7;
+            this.Softdrink.Text = "Softdrink";
+            // 
+            // Fries
+            // 
+            this.Fries.AutoSize = true;
+            this.Fries.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fries.Location = new System.Drawing.Point(49, 161);
+            this.Fries.Name = "Fries";
+            this.Fries.Size = new System.Drawing.Size(110, 20);
+            this.Fries.TabIndex = 9;
+            this.Fries.Text = "French Fries";
+            // 
+            // FBox
+            // 
+            this.FBox.Location = new System.Drawing.Point(204, 161);
+            this.FBox.Name = "FBox";
+            this.FBox.Size = new System.Drawing.Size(35, 20);
+            this.FBox.TabIndex = 10;
+            // 
+            // Poutine
+            // 
+            this.Poutine.AutoSize = true;
+            this.Poutine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Poutine.Location = new System.Drawing.Point(89, 187);
+            this.Poutine.Name = "Poutine";
+            this.Poutine.Size = new System.Drawing.Size(70, 20);
+            this.Poutine.TabIndex = 11;
+            this.Poutine.Text = "Poutine";
+            // 
+            // PBox
+            // 
+            this.PBox.Location = new System.Drawing.Point(204, 187);
+            this.PBox.Name = "PBox";
+            this.PBox.Size = new System.Drawing.Size(35, 20);
+            this.PBox.TabIndex = 12;
+            // 
+            // SBox
+            // 
+            this.SBox.Location = new System.Drawing.Point(204, 135);
+            this.SBox.Name = "SBox";
+            this.SBox.Size = new System.Drawing.Size(35, 20);
+            this.SBox.TabIndex = 13;
+            // 
+            // calculateP
+            // 
+            this.calculateP.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.calculateP.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.calculateP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateP.Location = new System.Drawing.Point(28, 213);
+            this.calculateP.Name = "calculateP";
+            this.calculateP.Size = new System.Drawing.Size(210, 30);
+            this.calculateP.TabIndex = 14;
+            this.calculateP.Text = "CALCULATE PRICE";
+            this.calculateP.UseVisualStyleBackColor = true;
+            this.calculateP.Click += new System.EventHandler(this.calculateP_Click);
+            // 
+            // subTlabel
+            // 
+            this.subTlabel.AutoSize = true;
+            this.subTlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTlabel.Location = new System.Drawing.Point(32, 256);
+            this.subTlabel.Name = "subTlabel";
+            this.subTlabel.Size = new System.Drawing.Size(80, 18);
+            this.subTlabel.TabIndex = 15;
+            this.subTlabel.Text = "Sub Total";
+            // 
+            // stPlabel
+            // 
+            this.stPlabel.AutoSize = true;
+            this.stPlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stPlabel.Location = new System.Drawing.Point(214, 256);
+            this.stPlabel.Name = "stPlabel";
+            this.stPlabel.Size = new System.Drawing.Size(0, 18);
+            this.stPlabel.TabIndex = 16;
+            // 
+            // taxlabel
+            // 
+            this.taxlabel.AutoSize = true;
+            this.taxlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxlabel.Location = new System.Drawing.Point(73, 284);
+            this.taxlabel.Name = "taxlabel";
+            this.taxlabel.Size = new System.Drawing.Size(35, 18);
+            this.taxlabel.TabIndex = 17;
+            this.taxlabel.Text = "Tax";
+            // 
+            // tPlabel
+            // 
+            this.tPlabel.AutoSize = true;
+            this.tPlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tPlabel.Location = new System.Drawing.Point(214, 284);
+            this.tPlabel.Name = "tPlabel";
+            this.tPlabel.Size = new System.Drawing.Size(0, 18);
+            this.tPlabel.TabIndex = 18;
+            // 
+            // totallabel
+            // 
+            this.totallabel.AutoSize = true;
+            this.totallabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totallabel.Location = new System.Drawing.Point(62, 318);
+            this.totallabel.Name = "totallabel";
+            this.totallabel.Size = new System.Drawing.Size(46, 18);
+            this.totallabel.TabIndex = 19;
+            this.totallabel.Text = "Total";
+            // 
+            // totalPlabel
+            // 
+            this.totalPlabel.AutoSize = true;
+            this.totalPlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalPlabel.Location = new System.Drawing.Point(214, 318);
+            this.totalPlabel.Name = "totalPlabel";
+            this.totalPlabel.Size = new System.Drawing.Size(0, 18);
+            this.totalPlabel.TabIndex = 20;
+            // 
+            // spacelabel
+            // 
+            this.spacelabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.spacelabel.Location = new System.Drawing.Point(32, 347);
+            this.spacelabel.Name = "spacelabel";
+            this.spacelabel.Size = new System.Drawing.Size(218, 10);
+            this.spacelabel.TabIndex = 21;
+            // 
+            // tenderedlabel
+            // 
+            this.tenderedlabel.AutoSize = true;
+            this.tenderedlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedlabel.Location = new System.Drawing.Point(32, 369);
+            this.tenderedlabel.Name = "tenderedlabel";
+            this.tenderedlabel.Size = new System.Drawing.Size(78, 18);
+            this.tenderedlabel.TabIndex = 22;
+            this.tenderedlabel.Text = "Tendered";
+            // 
+            // dollarlabel
+            // 
+            this.dollarlabel.AutoSize = true;
+            this.dollarlabel.Location = new System.Drawing.Point(182, 374);
+            this.dollarlabel.Name = "dollarlabel";
+            this.dollarlabel.Size = new System.Drawing.Size(13, 13);
+            this.dollarlabel.TabIndex = 23;
+            this.dollarlabel.Text = "$";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(204, 369);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(35, 20);
+            this.tenderedInput.TabIndex = 24;
+            // 
+            // calchange
+            // 
+            this.calchange.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.calchange.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.calchange.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calchange.Location = new System.Drawing.Point(28, 400);
+            this.calchange.Name = "calchange";
+            this.calchange.Size = new System.Drawing.Size(210, 30);
+            this.calchange.TabIndex = 25;
+            this.calchange.Text = "CALCULATE CHANGE";
+            this.calchange.UseVisualStyleBackColor = true;
+            this.calchange.Click += new System.EventHandler(this.calchange_Click);
+            // 
+            // changelabel
+            // 
+            this.changelabel.AutoSize = true;
+            this.changelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changelabel.Location = new System.Drawing.Point(32, 444);
+            this.changelabel.Name = "changelabel";
+            this.changelabel.Size = new System.Drawing.Size(65, 18);
+            this.changelabel.TabIndex = 26;
+            this.changelabel.Text = "Change";
+            // 
+            // changeP
+            // 
+            this.changeP.AutoSize = true;
+            this.changeP.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeP.Location = new System.Drawing.Point(214, 444);
+            this.changeP.Name = "changeP";
+            this.changeP.Size = new System.Drawing.Size(0, 18);
+            this.changeP.TabIndex = 27;
+            // 
+            // Printbutton
+            // 
+            this.Printbutton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Printbutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.Printbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Printbutton.Location = new System.Drawing.Point(28, 474);
+            this.Printbutton.Name = "Printbutton";
+            this.Printbutton.Size = new System.Drawing.Size(210, 30);
+            this.Printbutton.TabIndex = 28;
+            this.Printbutton.Text = "PRINT RECEIPT";
+            this.Printbutton.UseVisualStyleBackColor = true;
+            this.Printbutton.Click += new System.EventHandler(this.Printbutton_Click);
+            // 
+            // EBCash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 516);
+            this.Controls.Add(this.Printbutton);
+            this.Controls.Add(this.changeP);
+            this.Controls.Add(this.changelabel);
+            this.Controls.Add(this.calchange);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.dollarlabel);
+            this.Controls.Add(this.tenderedlabel);
+            this.Controls.Add(this.spacelabel);
+            this.Controls.Add(this.totalPlabel);
+            this.Controls.Add(this.totallabel);
+            this.Controls.Add(this.tPlabel);
+            this.Controls.Add(this.taxlabel);
+            this.Controls.Add(this.stPlabel);
+            this.Controls.Add(this.subTlabel);
+            this.Controls.Add(this.calculateP);
+            this.Controls.Add(this.SBox);
+            this.Controls.Add(this.PBox);
+            this.Controls.Add(this.Poutine);
+            this.Controls.Add(this.FBox);
+            this.Controls.Add(this.Fries);
+            this.Controls.Add(this.Softdrink);
+            this.Controls.Add(this.BBox);
+            this.Controls.Add(this.beefB);
+            this.Controls.Add(this.CBox);
+            this.Controls.Add(this.chickenB);
+            this.Controls.Add(this.Titlelabel);
+            this.Controls.Add(this.EBlabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EBCash";
+            this.Text = "EatBurger Cash Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EBlabel;
+        private System.Windows.Forms.Label Titlelabel;
+        private System.Windows.Forms.Label chickenB;
+        private System.Windows.Forms.TextBox CBox;
+        private System.Windows.Forms.Label beefB;
+        private System.Windows.Forms.TextBox BBox;
+        private System.Windows.Forms.Label Softdrink;
+        private System.Windows.Forms.Label Fries;
+        private System.Windows.Forms.TextBox FBox;
+        private System.Windows.Forms.Label Poutine;
+        private System.Windows.Forms.TextBox PBox;
+        private System.Windows.Forms.TextBox SBox;
+        private System.Windows.Forms.Button calculateP;
+        private System.Windows.Forms.Label subTlabel;
+        private System.Windows.Forms.Label stPlabel;
+        private System.Windows.Forms.Label taxlabel;
+        private System.Windows.Forms.Label tPlabel;
+        private System.Windows.Forms.Label totallabel;
+        private System.Windows.Forms.Label totalPlabel;
+        private System.Windows.Forms.Label spacelabel;
+        private System.Windows.Forms.Label tenderedlabel;
+        private System.Windows.Forms.Label dollarlabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Button calchange;
+        private System.Windows.Forms.Label changelabel;
+        private System.Windows.Forms.Label changeP;
+        private System.Windows.Forms.Button Printbutton;
+    }
+}
+
---
+++ EB Cash/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EB_Cash
+{
+    public partial class EBCash : Form
+    {
+        const double CHICKEN_PRICE = 5.5;
+        int chicken = 0;
+        const double BEEF_PRICE = 6.0;
+        int beef = 0;
+        const double SOFTDRINK_PRICE = 5.5;
+        int softdrink = 0;
+        const double FRIES_PRICE = 5.5;
+        int fries = 0;
+        const double POUTINE_PRICE = 5.5;
+        int poutine = 0;
+        const double TAX_COST = 0.13;
+        double total, taxCost, priceWithTax,subtotal, tendered, change;
+
+        
+
+        private void calculateP_Click(object sender, EventArgs e)
+        {
+            //put in all the constants and doubles
+            chicken = Convert.ToInt32(CBox.Text);
+            beef = Convert.ToInt32(BBox.Text);
+            softdrink = Convert.ToInt32(SBox.Text);
+            fries = Convert.ToInt32(FBox.Text);
+            poutine = Convert.ToInt32(PBox.Text);
+
+            //calculate the subtotal, taxcost and price with tax
+            subtotal = beef * BEEF_PRICE + chicken * CHICKEN_PRICE + softdrink * SOFTDRINK_PRICE + fries * FRIES_PRICE + poutine * POUTINE_PRICE;
+            stPlabel.Text = subtotal.ToString("C");
+            taxCost = subtotal * TAX_COST;
+            tPlabel.Text = taxCost.ToString("C");
+            priceWithTax = subtotal + taxCost;
+            totalPlabel.Text = priceWithTax.ToString("C");
+
+        }
+            //set the tendered up and let it calculate the change
+        private void calchange_Click(object sender, EventArgs e)
+        {
+            tendered = Convert.ToDouble(tenderedInput.Text);
+            change = tendered - priceWithTax;
+            changeP.Text = change.ToString("C");
+        }
+
+        //set the receipt printer up and creat a receipt
+        private void Printbutton_Click(object sender, EventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Black, 2);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            formGraphics.DrawLine(drawPen, 200, 200, 100, 100);
+           
+        }
+
+        public EBCash()
+        {
+            InitializeComponent();
+        }
+
+       
+
+
+    }
+    }
+
